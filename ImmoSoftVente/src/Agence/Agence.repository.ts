453eeae56d4171f5435f07f182fbe3523568762a
Valentin <valentin.barriol@ynov.com>
2019-03-27 12:25:01 +0100

    Projet

--- ImmoSoftVente/src/Agence/Agence.repository.ts
+++
@@ -1,6 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-
-import { Agence } from './Agence.entity';
-
-@EntityRepository(Agence)
-export class AgenceRepository extends Repository<Agence> {}
