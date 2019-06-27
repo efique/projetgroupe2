47940a1422d86fb21a612266ffaf9c0b3d2e08c8
HeleneBtx <helene.boiteux@ynov.com>
2019-06-27 14:55:37 +0200

    routing 3

--- Front/troisiemeproj-bootstrap/src/app/auth/auth.guard.ts
+++ Front/troisiemeproj-bootstrap/src/app/auth/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, CanActivateChild, CanLoad, Route, UrlSegment } from '@angular/router';
+import { CanActivate, CanActivateChild, CanLoad, Route, Router, UrlSegment } from '@angular/router';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
@@ -7,11 +7,16 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
+
+  constructor(private router: Router) { }
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     console.log('AuthGuard#canActivate called');
     return true;
+    // this.router.navigate(['/connexion']);
+    // return false;
   }
   canActivateChild(
     next: ActivatedRouteSnapshot,
